e38091aa16f38f4f5f66f046db0fde008d0da681
NANikolaev <91592401+NANikolaev@users.noreply.github.com>
2022-04-23 14:12:35 +0300

    add create view and func

--- src/controllers/estate-controller.js
+++ src/controllers/estate-controller.js
@@ -6,6 +6,14 @@ router.get('/houses-for-rent',(req,res)=>{
        .then(house=>{res.render('aprt-for-recent',{house})})
 })
 
+router.route('/create-offer')
+.get((req,res)=>{
+    res.render('create')
+})
+.post((req,res)=>{
+    estateService.create(req,res)
+    .then(house=>res.redirect('/houses-for-rent'))
+})
 
 
 
--- src/services/estate-service.js
+++ src/services/estate-service.js
@@ -1,9 +1,15 @@
 const Estate=require('../models/Estate');
 
 function getAll(req,res){
-    return Estate.find()
+    return Estate.find().lean()
+}
+
+function create(req,res){
+    req.body.owner=req.user.find
+    return Estate.create(req.body)
 }
 
 module.exports={
-    getAll
+    getAll,
+    create
 }
