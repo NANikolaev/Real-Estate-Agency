dbbc5fd8452274dcf558f63c10fa9b8b4190f20c
NANikolaev <91592401+NANikolaev@users.noreply.github.com>
2022-04-24 17:11:10 +0300

    Update Estate.js

--- src/models/Estate.js
+++ src/models/Estate.js
@@ -4,31 +4,39 @@ const User=require('./User');
 const estateSchema=new mongoose.Schema({
      name:{
          type:String,
-         required:true,
+         required:[true,"Name is required"],
+         minlength:[6,"Name should be at least 6 characters long."]
      },
      type:{
         type:String,
-        required:true,
+        required:[true,"Type is required"],
      },
      year:{
         type:Number,
-        required:true,
+        required:[true,"Type is required"],
+        min:[1850,"Building shouldnt be under 1850 year."],
+        max:[2022,"Unexisting building."]
      },
      city:{
         type:String,
-        required:true,
+        required:[true,"City is required"],
+        minlength:[4,"City name should be at least 5 characters long."]
      },
      image:{
         type:String,
-        required:true,
+        required:[true,"House image is required"],
+        validate:[/^[http?s:\/\/]+/,'Invalid Url adress']
      },
      description:{
         type:String,
-        required:true,
+        required:[true,"Description is required"],
+        maxlength:[60,'Description should be maximum of 60 characters long.']
      },
      pieces:{
         type:Number,
-        required:true,
+        required:[true,"Available pieces is required"],
+        min:[0,'Pieces cant be minus'],
+        max:[10,'Max Pieces 10']
      },
      rentedUsers:[{type:mongoose.Schema.Types.ObjectId,ref:'User'}],
      owner:{type:mongoose.Schema.Types.ObjectId,ref:'User'}
